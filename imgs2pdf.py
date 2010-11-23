3ab2a8e9b8982714c7de009d1e8238f443fd023b
Raúl Soriano <GatoLoko@gmail.com>
2010-11-23 15:29:01 +0100

    * Changed version number to 1.2. * Modified all parts to work with an in memory, opened image, to avoid   opening/closing multiple times the same image. * Added "-R" and "--rotate" to rotate the images before placing them   on the pdf file. * Modified the entire script to show messages before doing things, so   if something takes a long time or crashes we know where it is.

--- imgs2pdf.py
+++ imgs2pdf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF8 -*-
 #       
-# Copyright 2009 Raul Soriano <GatoLoko@gmail.com>
+# Copyright 2009, 2010 Raul Soriano <GatoLoko@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,15 +20,16 @@
 
 '''Usage: imgs2pdf [OPTIONS...]
 
-  -c, --compress             Enables PDF text compression
-  -h, --help                 Shows this help
-  -i, --inline               Use "inline" images instead of "external".
-                             This can be faster, but generates bigger
-                             files and may cause problems
-  -o NAME, --output=NAME     Sets the name of the generated PDF 
-  -r, --resize               Resize the pictures to fit in A4 page size
-  -t TITLE, --title=TITLE    Sets the PDF title
-  -v, --version              Shows imgs2pdf version
+  -c, --compress               Enables PDF text compression
+  -h, --help                   Shows this help
+  -i, --inline                 Use "inline" images instead of "external".
+                               This can be faster, but generates bigger
+                               files and may cause problems
+  -o NAME, --output=NAME       Sets the name of the generated PDF 
+  -r, --resize                 Resize the pictures to fit in A4 page size
+  -R DEGREE, --rotate=DEGREE   Rotate the pictures 
+  -t TITLE, --title=TITLE      Sets the PDF title
+  -v, --version                Shows imgs2pdf version
 '''
 
 from __future__ import division
@@ -38,9 +39,9 @@ import getopt
 from PIL import Image
 from reportlab.pdfgen import canvas
 
-__VERSION__ = "1.1"
+__VERSION__ = "1.2"
 EXTENSIONES = [ ".jpg", ".gif", ".png", ".JPG", ".GIF", ".PNG" ]
-# Reportlab A4 pages have a ridiculously small resolution of 595h x 842v
+# Reportlab A4 pages have a ridiculously small resolution of 595w x 842h
 ANCHOPDF = 595
 ALTOPDF = 842
 RESIZEA4 = False
@@ -48,20 +49,21 @@ COMPRESION = 0
 INLINE = 0
 TITULO = "Titulo"
 SALIDA = "salida.pdf"
+DEGREE = 0
 
 
 def redimensiona(imagen):
     """Return an scaled copy of the supplied image."""
-    imagenorig = Image.open(imagen)
-    ancho, alto = imagenorig.size
+    ancho, alto = imagen.size
     if (alto / ancho) > (ALTOPDF / ANCHOPDF):
         factor = ALTOPDF / alto
     else:
         factor = ANCHOPDF / ancho
-    print("A %s resizing factor will be aplied to %s" % (factor, imagen))
+    print("    Applying a resizing factor of %s" % factor)
+    imagen = imagen.resize((int(ancho * factor), int(alto * factor)), \
+                            Image.NEAREST)
     # Resizing modes: NEAREST, BILINEAR, BICUBIC, ANTIALIAS
-    return imagenorig.resize((int(ancho * factor), int(alto * factor)), \
-            Image.NEAREST)
+    return imagen
 
 def listaimagenes():
     """Return a list of image files from the working directory."""
@@ -75,10 +77,10 @@ def listaimagenes():
 
 def main():
     """Script main function."""
-    global RESIZEA4, TITULO, SALIDA, COMPRESION, INLINE
+    global RESIZEA4, TITULO, SALIDA, COMPRESION, INLINE, DEGREE
     try:
-        opcs, args = getopt.getopt(argv[1:], "chio:rt:v", ["compress", \
-                "help", "inline", "output=", "resize", "title=", \
+        opcs, args = getopt.getopt(argv[1:], "chio:rR:t:v", ["compress", \
+                "help", "inline", "output=", "resize", "rotate=","title=", \
                 "version"])
     except getopt.GetoptError:
         print(__doc__)
@@ -96,10 +98,12 @@ def main():
             SALIDA = arg
         elif opc in ("-r", "--resize"):
             RESIZEA4 = True
+        elif opc in ("-R", "--rotate"):
+            DEGREE = int(arg)
         elif opc in ("-t", "--title"):
             TITULO = arg
         elif opc in ("-v", "--version"):
-            print("imgs2pdf version %s" % __VERSION__)
+            print("imgs2pdf %s" % __VERSION__)
             exit(1)
 
     imagenes = listaimagenes()
@@ -108,35 +112,43 @@ def main():
     pdf.setPageCompression(COMPRESION)
 
     for imagen in imagenes:
+        print("Proccesing %s" % imagen)
+        # Open the image file
+        imagefile = Image.open(imagen)
+
+        # If a rotation is indicated, rotate the image
+        if DEGREE != 0:
+            print("    Rotating %s degrees" % str(DEGREE))
+            imagefile = imagefile.rotate(DEGREE, expand=1)
+            
         if RESIZEA4 is True:
             # Resize, align and print the picture in the A4 page
-            print("Proccesing %s" % imagen)
             # Resizing
-            imagenpdf = redimensiona(imagen)
+            imagenpdf = redimensiona(imagefile)
             # Calculate the page margins to center the picture
             margenhoriz = (ANCHOPDF - imagenpdf.size[0]) / 2
             margenvert = (ALTOPDF - imagenpdf.size[1]) / 2
             # Draw the picture centered in the current page
             if INLINE == 0:
-                pdf.drawImage(imagenpdf, margenhoriz, margenvert, \
-                        preserveAspectRatio=True)
+                pdf.drawImage(canvas.ImageReader(imagenpdf), \
+                        margenhoriz, margenvert, \
+                        preserveAspectRatio=True, anchor='c')
             else:
-                pdf.drawInlineImage(imagenpdf, margenhoriz, \
-                        margenvert, preserveAspectRatio=True)
+                pdf.drawInlineImage(canvas.ImageReader(imagenpdf), \
+                        margenhoriz, margenvert, \
+                        preserveAspectRatio=True, anchor='c')
         else:
             # Resize each page to fit the image size
-            print("Proccesing %s" % imagen)
-            # Open the image
-            imagenorig = Image.open(imagen)
             # Resize the page
-            print(imagenorig.size)
-            pdf.setPageSize(imagenorig.size)
+            print("    Resizing page to %s width and %s height" % imagefile.size)
+            pdf.setPageSize(imagefile.size)
             # Draw the original image in the current page
             if INLINE == 0:
-                pdf.drawImage(imagen, 0, 0, preserveAspectRatio=True)
-            else:
-                pdf.drawInlineImage(imagen, 0, 0, \
+                pdf.drawImage(canvas.ImageReader(imagefile), 0, 0, \
                         preserveAspectRatio=True)
+            else:
+                pdf.drawInlineImage(canvas.ImageReader(imagefile), \
+                        0, 0, preserveAspectRatio=True)
         # Close the current page and create a new one
         pdf.showPage()
     # Cerramos el PDF
